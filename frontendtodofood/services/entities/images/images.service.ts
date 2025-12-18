e0496f37cde56dfe661f44d1ff1b6f58f7b308ae
maurogarzia <maurogarzia2@gmail.com>
2025-12-18 13:46:22 -0300

    fix: edit createImages and uodatedImages

--- frontendtodofood/services/entities/images/images.service.ts
+++ frontendtodofood/services/entities/images/images.service.ts
@@ -1,10 +1,13 @@
 import { deleted, getAll, getById, post, put } from "@/services/core/crud.service"
 import { IImage } from "@/types/models/Image.model"
 import { IRequestUser, IUser } from "@/types/models/Users.model"
+import { METHODS } from "http"
+import { headers } from "next/headers"
 
 // Despues hay que cambiar el archivo
 
 const BASE_IMAGES = `${process.env.NEXT_PUBLIC_BASE_URL}/image`
+const BASE_CLOUDINARY = `${process.env.NEXT_PUBLIC_BASE_URL}/upload`
 
 
 export const getAllImages= () : Promise<IImage[]> => {
@@ -15,13 +18,50 @@ export const getImageById = (id: number) : Promise<IImage> => {
     return getById<IImage>(BASE_IMAGES, id)
 }
 
-export const createImage = async(data : IImage) : Promise<IImage> =>{
-    return post<IImage, IImage>(BASE_IMAGES, data)
+export const createImage = async(file : File) => {
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+        const response = await fetch(BASE_CLOUDINARY, {
+            method: "POST",
+            body: file
+        })
+
+        if (!response.ok) throw new Error("Error al subir la imágen")
+
+        const data = await response.json()
+
+        return data
+    } catch (error) {
+        console.error('Error en create Images', error)
+        throw error
+    }
 }
 
-export const updatedImage = async(data : IImage, id: number) : Promise<IImage> => {
-    return put<IImage, IImage>(BASE_IMAGES, data, id)
+export const updatedImages = async (file: File, id: number) => {
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+        const response = await fetch(`${BASE_CLOUDINARY}/${id}`, {
+        method: "PUT",
+        body: formData,
+    })
+
+    if (!response.ok) {
+        throw new Error("Error al actualizar imagen")
+    }
+
+    const data = await response.json()
+
+    return data
+    } catch (error) {
+        console.error(`Error en updatedImages`, error)
+        throw error
+    }
 }
+
 
 export const deletedImage = async(id : number) : Promise<void> => {
     return deleted(BASE_IMAGES, id)
