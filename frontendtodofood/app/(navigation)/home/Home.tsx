860c0f14f52b94c267093e8403f021d17a6adf86
maurogarzia <maurogarzia2@gmail.com>
2025-12-19 16:31:29 -0300

    fix: edit to home.module.css and Home.tsx

--- frontendtodofood/app/(navigation)/home/Home.tsx
+++ frontendtodofood/app/(navigation)/home/Home.tsx
@@ -9,7 +9,7 @@ import image3 from '../../../assets/imagesCarrusel/image3.webp'
 import image4 from '../../../assets/imagesCarrusel/image4.webp'
 import img from '../../../assets/image.png'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface HomeProps{
     actuallyPromotions: IPromotion[]
@@ -36,21 +36,29 @@ function Home({actuallyPromotions} : HomeProps) {
         }
     }
 
+    
+    // Cambio de imagen
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(prev =>
+                prev === carruselArray.length - 1 ? 0 : prev + 1
+            );
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, [])
+    
+
+    
+
     return (
         <div className={style.home}>
-
-            {/* Imagen */}
-            <div className={style.billboard}>
-                <Image height={100} width={1450} alt='' src={img}/>
-            </div>
-
-            
 
             {/* Carrusel */}
 
             <div className={style.carrusel}>
-                <Image className={style.image} height={400} width={500} src={carruselArray[index]} alt=''/>
-                <Image className={style.imageResponsive} height={200} width={250} src={carruselArray[index]} alt=''/>
+                <Image className={style.image} key={index} height={400} width={500} src={carruselArray[index]} alt=''/>
+                
                 <div className={style.arrows}>
                     <p onClick={() => handleIndex('res')}>
                         <span className="material-symbols-outlined">arrow_left</span>
@@ -65,6 +73,9 @@ function Home({actuallyPromotions} : HomeProps) {
             <p className={style.title}>Último día de promociones</p>
 
             <div className={style.cards}>
+                {actuallyPromotions.length < 1 &&
+                    <p className={style.emptyPromotions}>No se encuentran promociones con vencimiento en dia de hoy</p>
+                }
                 {actuallyPromotions.map(promotion => 
                     <Card key={promotion.id} name={promotion.name} image={promotion.image}/>
                 ) }
